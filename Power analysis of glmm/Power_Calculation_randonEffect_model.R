f8db58434ebcbe75332555983810ceac4455c8d1
Nivedita Bhadra <43319262+BhadraNivedita@users.noreply.github.com>
2024-07-26 12:46:39 +0300

    Add files via upload

---
+++ Power analysis of glmm/Power_Calculation_randonEffect_model.R
@@ -0,0 +1,26 @@
+#simr: Power Analysis for Generalised Linear Mixed Models by Simulation
+#https://cran.r-project.org/web/packages/simr/index.html
+library(simr)
+model1 <- glmer(z ~ x + (1|g), family="poisson", data=simdata)
+summary(model1)
+
+fixef(model1)["x"]
+fixef(model1)["x"] <- -0.05
+powerSim(model1)
+
+## Increasing sample size
+model2 <- extend(model1, along="x", n=20)
+powerSim(model2)
+
+
+#Identifying the Minimum Sample Size Required
+pc2 <- powerCurve(model2)
+print(pc2)
+plot(pc2)
+
+##Varying the number and size of groups
+##Adding More Groups
+model3 <- extend(model1, along="g", n=15)
+pc3 <- powerCurve(model3, along="g")
+plot(pc3) 
+
